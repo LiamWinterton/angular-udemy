a4021c79f8329f255fc27353cbab006dac91c9c9
Liam <liam@dijitul.uk>
2021-03-06 15:01:03 +0000

    Bam, you can also set the innerHtml of an element with an attribute

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false;
+  addServerButtonText = 'Add Server';
 
   constructor() {
     setTimeout(() => {
