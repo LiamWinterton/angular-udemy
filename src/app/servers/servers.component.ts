d24b079e53e993c710c410ab5b17811aaa347eb2
Liam <liam@dijitul.uk>
2021-03-14 19:06:42 +0000

    Very simple data binding
    Up to this point, I definitely prefer reacts way of doing things, parenthesis on custom html tags feels weird

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,11 +7,15 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false;
-  addServerButtonText = 'Add Server';
+  serverCreationStatus = 'No Server was created.';
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created.';
   }
 }
