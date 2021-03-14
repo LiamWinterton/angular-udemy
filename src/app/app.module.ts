a663250fb220c37c4f4c8269d56f8d8d2989b8e2
Liam <liam@dijitul.uk>
2021-03-14 19:42:15 +0000

    Remove previous assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { AssignmentComponent } from './assignment/assignment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    AssignmentComponent
   ],
   imports: [
     BrowserModule,
